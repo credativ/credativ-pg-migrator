d616a749bd811b8d62e2b473ba7e3c6ae4adac3e
Michael Banck <michael.banck@credativ.de>
2025-04-17 16:20:33 +0200

    add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='credativ-pg-migrator-dev',
+    version='0.0',
+    url='https://github.com/credativ/credativ-pg-migrator-dev.git',
+    author='Josef Machytka',
+    author_email='josef.machytka@credativ.de',
+    description='Description of my package',
+    packages=find_packages(),
+    install_requires=['psycopg2', 'jaydebeapi', 'pyyaml', 'pandas', 'pyodbc'],
+)

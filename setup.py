89714b0c966c55024cc5bacc6fec83e08cbeb653
Michael Banck <michael.banck@credativ.de>
2025-06-18 09:38:30 +0200

    set version to 0.9.0rc1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='credativ-pg-migrator',
-    version='0.8.3dev',
+    version='0.9.0rc1',
     url='https://github.com/credativ/credativ-pg-migrator.git',
     author='Josef Machytka',
     author_email='josef.machytka@credativ.de',
